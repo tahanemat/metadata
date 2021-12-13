591fba3e415ed356ad3ed1c51088a270351f93f7
Taha Nemat <tahanemat6@gmail.com>
2021-12-13 19:44:40 +0500

    add test and fix whitespace bug

--- test/test.cpp
+++ test/test.cpp
@@ -24,7 +24,7 @@ protected:
 };
 
 std::vector<std::string> split(std::string s, std::string delimiter) {
-  size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+  std::size_t pos_start = 0, pos_end, delim_len = delimiter.length();
   std::string token;
   std::vector<std::string> res;
 
@@ -48,11 +48,19 @@ Entries parseCout(std::string input) {
       entries.emplace_back();
       for (const auto line : lines) {
         const auto keyval = split(line, ": ");
-        entries.back().insert(std::make_pair(keyval.front(), keyval.back()));
+        auto key = keyval.front();
+        key.erase(remove_if(key.begin(), key.end(), isspace), key.end());
+        entries.back().insert(std::make_pair(key, keyval.back()));
       }
     }
   }
   return entries;
+}
+
+void createFile(std::filesystem::path p, std::string text_to_insert = "abc\n") {
+  std::ofstream ofs(p);
+  ofs << text_to_insert;
+  ofs.close();
 }
 
 TEST_F(FilesystemTest, Test_Directory_Print) {
@@ -81,7 +89,6 @@ TEST_F(FilesystemTest, Test_File_Print) {
 
   std::filesystem::directory_entry e{"./" + test_dir_name + "/"};
   std::filesystem::path path{"./" + test_dir_name + "/testfile.txt"};
-  // path /= "testfile.txt";
 
   std::ofstream ofs(path);
   ofs << "this is some text in the new file\n";
@@ -110,9 +117,10 @@ TEST_F(FilesystemTest, Test_Symlink_Ignore) {
 
   std::filesystem::directory_entry e{"./" + test_dir_name + "/"};
 
-  std::filesystem::create_symlink("./" + test_dir_name + "/", "./example");
+  std::filesystem::create_symlink("./" + test_dir_name + "/",
+                                  "./" + test_dir_name + "/symlink");
 
-  display_all_files_in_entry(e);
+  print_directory(e);
 
   std::string output = testing::internal::GetCapturedStdout();
 
@@ -127,4 +135,49 @@ TEST_F(FilesystemTest, Test_Symlink_Ignore) {
   EXPECT_EQ(entry.at("Files"), "0");
   EXPECT_EQ(entry.at("Directories"), "0");
   EXPECT_EQ(entry.at("Bytes"), "0");
+}
+
+TEST_F(FilesystemTest, Test_SubDirectories_With_Files) {
+
+  ::testing::internal::CaptureStdout();
+
+  // GENERATE FILES AND SUBDIRECTORIES====================================
+  std::filesystem::directory_entry e{"./" + test_dir_name + "/"};
+
+  createFile("./" + test_dir_name + "/testfile1.txt");
+  createFile("./" + test_dir_name + "/testfile2.txt");
+  std::filesystem::create_directories("./" + test_dir_name + "/subdir1/");
+  std::filesystem::create_directories("./" + test_dir_name + "/subdir2/");
+  createFile("./" + test_dir_name + "/subdir1/testfile3.txt");
+  createFile("./" + test_dir_name + "/subdir2/testfile4.txt");
+  //======================================================================
+
+  print_directory(e);
+  display_all_files_in_entry(e);
+
+  const std::string output = testing::internal::GetCapturedStdout();
+
+  const auto nodes = parseCout(output);
+
+  EXPECT_EQ(nodes.size(), 7);
+
+  for (const auto node : nodes) {
+    const auto node_path = node.at("Path");
+
+    if (node_path == "/") {
+      EXPECT_EQ(node.at("Type"), "Directory");
+      EXPECT_EQ(node.at("Files"), "2");
+      EXPECT_EQ(node.at("Directories"), "2");
+      EXPECT_GT(node.at("Bytes"), "0");
+    } else if (node_path == "testfile1.txt" || node_path == "testfile2.txt" ||
+               node_path == "testfile3.txt" || node_path == "testfile4.txt") {
+      EXPECT_EQ(node.at("Type"), "File");
+      EXPECT_GT(node.at("Bytes"), "0");
+    } else if (node_path == "subdir1/" || node_path == "subdir2/") {
+      EXPECT_EQ(node.at("Type"), "Directory");
+      EXPECT_EQ(node.at("Files"), "1");
+      EXPECT_EQ(node.at("Directories"), "0");
+      EXPECT_GT(node.at("Bytes"), "0");
+    }
+  }
 }
